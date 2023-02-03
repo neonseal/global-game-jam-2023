1a8694bd91cbc3d8496473de1f8fa092a899fe82
Corey Sieland-Harris <corey.sielandharris@gmail.com>
2023-02-03 00:51:55 -0500

    2.5D Asset drag and drop instantiation functional

--- Assets/Scripts/ItemDropHandler.cs
+++ Assets/Scripts/ItemDropHandler.cs
@@ -5,20 +5,35 @@ using UnityEngine.EventSystems;
 
 public class ItemDropHandler : MonoBehaviour, IDropHandler
 {
-    public Inventory _Inventory;
+    [SerializeField] private CursorManager cursorManager;
+
+    private void Awake() {
+        cursorManager = new CursorManager();
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
         RectTransform invPanel = transform as RectTransform;
 
         if (!RectTransformUtility.RectangleContainsScreenPoint(invPanel, Input.mousePosition))
         {
-            IInventoryItem item = eventData.pointerDrag.gameObject.GetComponent<ItemDragHandler>().Item;
-            if (item != null)
-            {
-                _Inventory.RemoveItem(item);
-                item.OnDrop();
+            GameObject[] items = eventData.pointerDrag.gameObject.GetComponent<ItemDragHandler>().inventoryItems;
+            if (items != null) {
+                GameObject selectedTile = cursorManager.CalculateRayCastHitGameObject();
+                PlaceGameObject(items, selectedTile.transform.position);
             }
-
         }
+    }
+
+    private void PlaceGameObject(GameObject[] inventoryItems, Vector3 tilePosition) {
+        int selectedItemIndex = 0;
+        if (inventoryItems.Length > 1) {
+            selectedItemIndex = Random.Range(0, inventoryItems.Length - 1);
+        }
+        Vector3 itemPosition = new Vector3(tilePosition.x, tilePosition.y + 2, tilePosition.z);
+        Debug.Log(itemPosition);
+        Transform parentTransform = GameObject.FindGameObjectWithTag("ComponentLayer").transform;
+        Quaternion rotation = Quaternion.LookRotation(-Camera.main.transform.forward);
+        Instantiate(inventoryItems[selectedItemIndex], itemPosition, rotation, parentTransform);
     }
 }
