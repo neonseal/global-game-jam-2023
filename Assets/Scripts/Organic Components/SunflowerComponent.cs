c2f30dd2612408072a83e3051ff7dff657abbacd
Corey Sieland-Harris <corey.sielandharris@gmail.com>
2023-02-05 12:53:52 -0500

    Add updated camera

--- Assets/Scripts/Organic Components/SunflowerComponent.cs
+++ Assets/Scripts/Organic Components/SunflowerComponent.cs
@@ -5,17 +5,15 @@ using ForestComponent;
 
 public class SunflowerComponent : MonoBehaviour, IForestComponent {
     private ResourceGenerator resourceGenerator;
-    [SerializeField] public float maintenanceCost { get; set; } = 75f;
+    [SerializeField] public float maintenanceCost { get; set; } = 10f;
     [SerializeField] public float energyBuildCost { get; set; } = 0f;
-    [SerializeField] public float waterBuildCost { get; set; } = 50f;
-    [SerializeField] public float organicBuildCost { get; set; } = 50f;
+    [SerializeField] public float waterBuildCost { get; set; } = 5f;
+    [SerializeField] public float organicBuildCost { get; set; } = 5f;
     [SerializeField] public float maxHealth { get; set; } = 100;
     [SerializeField] public float health { get; set; }
 
     private void Awake() {
         resourceGenerator = new ResourceGenerator();
-        resourceGenerator.AmountPerCycle = 40.0f;
-        resourceGenerator.BaseAmountPerCycle = 40.0f;
         health = maxHealth;
     }
 
